fba4763850f2fa7db4e5e7de2f3f7d5e50f7f8e7
HewstonFox <hewstonfox@gmail.com>
2020-09-23 02:21:35 +0300

    Pipfile

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DB_URL = os.environ.get('MONGODB_URL') or 'mongodb://mrHeroku:Dg549SGfnRbg5WML@kbinderdb.ztsdv.mongodb.net' \
+DB_URL = os.environ.get('MONGODB_URL') or 'mongodb+srv://mrHeroku:Dg549SGfnRbg5WML@kbinderdb.ztsdv.mongodb.net' \
                                           '/kBinderDB?retryWrites=true&w=majority'
 TOKEN = '1028373326:AAERuI4AxtIrejLvk-TQBwTY_k0iCkXMzxw'
 PORT = 27017
